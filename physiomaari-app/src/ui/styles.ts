a7c953f35dbb649fcdc9c7c75ce4d0489ca82b6c
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-04-07 13:49:38 +0300

    lisätty tyylejä

--- physiomaari-app/src/ui/styles.ts
+++ physiomaari-app/src/ui/styles.ts
@@ -1,5 +1,7 @@
 import { StyleSheet } from "react-native";
 import { colors } from "./colors";
+import { spacing } from "./spacing";
+import { typography } from "./typography";
 
 export const styles = StyleSheet.create({
   bottomTabBar: {
@@ -7,5 +9,21 @@ export const styles = StyleSheet.create({
     height: 100,
     elevation: 0,
     shadowOpacity: 0,
+  },
+  profileInfo: {
+    backgroundColor: colors.secondary,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: spacing.sm,
+    padding: 10,
+    borderRadius: 10,
+  },
+  profileScreenBackground: {
+    backgroundColor: colors.background,
+  },
+  userList: {
+    fontSize: typography.fontSize.medium,
+    marginLeft: 20,
+    padding: 10,
   },
 });
