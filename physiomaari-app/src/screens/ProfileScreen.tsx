5e1ea60543a26dfe8b162c016bff6ca29cfc87d1
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-04-07 13:48:51 +0300

    testattu aktiivisen käyttäjän tietojen näkymistä profiili-näkymässä

--- physiomaari-app/src/screens/ProfileScreen.tsx
+++ physiomaari-app/src/screens/ProfileScreen.tsx
@@ -1,5 +1,19 @@
-import { Text } from "react-native-paper";
+import { Surface, Text } from "react-native-paper";
+import { useUser } from "../hooks/UserContext";
+import { View } from "react-native";
+import { styles } from "../ui/styles";
 
 export default function ProfileScreen() {
-  return <Text>Information about user will be listed here</Text>;
+  const { user } = useUser();
+
+  return (
+    <Surface style={styles.profileInfo}>
+      <Text>
+        Name: {user?.firstName} {user?.lastName}{" "}
+      </Text>
+      <Text>Username: {user?.username} </Text>
+      <Text>Email: {user?.email} </Text>
+      <Text>Role: {user?.role} </Text>
+    </Surface>
+  );
 }
