2d94f36d08ce9c205548cb58583de4db0ac3b269
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-04-07 12:32:18 +0300

    muokattu tyylitiedostoa ja lisätty colors, spacing ja typography jatkon tyylittelyä varten

---
+++ physiomaari-app/src/ui/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: "#003580",
+  secondary: "rgba(0, 53, 128, 0.5)",
+  background: "#FAF9F6",
+  white: "#FFFFFF",
+  text: "#000000",
+  gray: "#888888",
+};
---
+++ physiomaari-app/src/ui/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./colors";
+
+export const styles = StyleSheet.create({
+  bottomTabBar: {
+    backgroundColor: colors.secondary,
+    height: 100,
+    elevation: 0,
+    shadowOpacity: 0,
+  },
+});
