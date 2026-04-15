1dad9bec98c7644bdc62309e1b790271b1ef086d
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-04-15 10:10:03 +0300

    added date to exercise and updated demodata

--- physiomaari-app/demodata.ts
+++ physiomaari-app/demodata.ts
@@ -1,3 +1,9 @@
+import {
+  TrainingSession,
+  SessionStatus,
+  SessionExercise,
+  ExerciseStatus,
+} from "./src/types/Exercise";
 import { AppUser } from "./src/types/User";
 
 export const demoUsers: AppUser[] = [
@@ -27,5 +33,84 @@ export const demoUsers: AppUser[] = [
     username: "jukkal",
     passwordHashed: "hashed_password_3",
     role: "user",
+  },
+];
+
+export const demoTrainingSessions: TrainingSession[] = [
+  {
+    userId: 1,
+    sessionId: 1,
+    title: "DAY 1",
+    description:
+      "DAY 1. Consists of warm up session, strength session (back squat + pull up).",
+    datePlanned: "2026-05-05",
+    status: "upcoming",
+    exercises: [
+      {
+        exerciseId: 1,
+        title: "1. Warm up",
+        description:
+          "General warm up: 5 minutes on machine/running. Then 2 rounds: ....",
+        status: "upcoming",
+      },
+      {
+        exerciseId: 2,
+        title: "2. Strength",
+        description:
+          "3x3 back aquat and pull ups alternating. 2 min rest between rounds",
+        status: "upcoming",
+      },
+    ],
+  },
+  {
+    userId: 1,
+    sessionId: 1,
+    title: "DAY 2",
+    description:
+      "DAY 2. Consists of warm up session, strength session (deadlit + bench press).",
+    datePlanned: "2026-05-07",
+    status: "upcoming",
+    exercises: [
+      {
+        exerciseId: 1,
+        title: "1. Warm up",
+        description:
+          "General warm up: 5 minutes on machine/running. Then 2 rounds: ....",
+        status: "upcoming",
+      },
+      {
+        exerciseId: 2,
+        title: "2. Strength",
+        description:
+          "3x3 deadlit and bench press alternating. 2 min rest between rounds",
+        status: "upcoming",
+      },
+    ],
+  },
+
+  {
+    userId: 1,
+    sessionId: 1,
+    title: "DAY 3",
+    description:
+      "DAY 2. Consists of warm up session, strength session (deadlit + bench press).",
+    datePlanned: "2026-04-05",
+    status: "completed",
+    exercises: [
+      {
+        exerciseId: 1,
+        title: "1. Warm up",
+        description:
+          "General warm up: 5 minutes on machine/running. Then 2 rounds: ....",
+        status: "completed",
+      },
+      {
+        exerciseId: 2,
+        title: "2. Strength",
+        description:
+          "3x10 deadlit and bench press alternating. 1,5 min rest between rounds",
+        status: "completed",
+      },
+    ],
   },
 ];
--- physiomaari-app/src/types/Exercise.ts
+++ physiomaari-app/src/types/Exercise.ts
@@ -2,19 +2,21 @@ export type SessionStatus = "completed" | "upcoming";
 export type ExerciseStatus = "completed" | "upcoming";
 
 export type TrainingSession = {
-  userId: string;
-  sessionId: string;
+  userId?: number; //muutetaan pakolliseksi, kun kanta ja testikäyttäjät käytössä
+  sessionId: number;
   title: string;
   description: string;
+  datePlanned: string;
   status: SessionStatus;
   exercises: SessionExercise[];
 };
 
 export type SessionExercise = {
-  exerciseId: string;
+  exerciseId: number;
   title: string;
   description: string;
   status: ExerciseStatus;
+  videoUrl?: string; //valmentajan lisäämä youtube-linkki, valinnainen
 };
 
 export type UserComment = {
