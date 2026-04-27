3dd811225c9586bd24d3c2512944d07067cddaa4
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-04-27 14:08:12 +0300

    commented on code, error handling

--- physiomaari-app/src/screens/ProfileScreen.tsx
+++ physiomaari-app/src/screens/ProfileScreen.tsx
@@ -2,14 +2,19 @@ import { Surface, Text } from "react-native-paper";
 import { useUsers } from "../hooks/UserContext";
 import { View } from "react-native";
 import { styles } from "../ui/styles";
+import { colors } from "../ui/colors";
 
 export default function ProfileScreen() {
+  //haetaan aktiivinen käyttäjä contextista
   const { activeUser } = useUsers();
 
+  if (!activeUser) {
+    return <Text style={{ color: colors.error }}>No active user</Text>;
+  }
   return (
     <Surface style={styles.profileInfo}>
       <Text>
-        Name: {activeUser.firstName} {activeUser.lastName}{" "}
+        Name: {activeUser.firstName} {activeUser.lastName}
       </Text>
       <Text>Username: {activeUser.username} </Text>
       <Text>Email: {activeUser.email} </Text>
