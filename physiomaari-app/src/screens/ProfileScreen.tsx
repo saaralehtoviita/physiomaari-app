75c0692ebce6f05cbb5447a5435475b76a60eca2
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-05-01 19:16:46 +0300

    improved styling

--- physiomaari-app/src/screens/ProfileScreen.tsx
+++ physiomaari-app/src/screens/ProfileScreen.tsx
@@ -1,10 +1,8 @@
-import { Button, Surface, Text } from "react-native-paper";
+import { Card, Surface, Text } from "react-native-paper";
 import { useUsers } from "../hooks/UserContext";
-import { View } from "react-native";
+
 import { styles } from "../ui/styles";
 import { colors } from "../ui/colors";
-import { logoutUser } from "../firebase/authService";
-import LoginScreen from "./LoginScreen";
 
 export default function ProfileScreen() {
   //haetaan aktiivinen käyttäjä contextista
@@ -18,15 +16,30 @@ export default function ProfileScreen() {
     await logoutUser();
   } */
   return (
-    <Surface style={styles.profileInfo}>
-      <Text>
-        Name: {activeUser.firstName} {activeUser.lastName}
-      </Text>
-      <Text>Username: {activeUser.username} </Text>
-      <Text>Email: {activeUser.email} </Text>
-      <Text>Role: {activeUser.role} </Text>
-      <Text>ID: {activeUser.appUserId}</Text>
+    <Card
+      mode="elevated"
+      style={{
+        backgroundColor: colors.backgroundDarker,
+        marginTop: 20,
+        marginLeft: 10,
+        marginRight: 10,
+      }}
+    >
+      <Card.Title title="User profile info" />
+
+      <Card.Content>
+        <Surface style={{ padding: 5, borderRadius: 5 }}>
+          <Text>
+            Name: {activeUser.firstName} {activeUser.lastName}
+          </Text>
+          <Text>Username: {activeUser.username} </Text>
+          <Text>Email: {activeUser.email} </Text>
+          <Text>Role: {activeUser.role} </Text>
+          <Text>ID: {activeUser.id}</Text>
+        </Surface>
+      </Card.Content>
+
       {/* <Button onPress={handleLogout}>Log out</Button> */}
-    </Surface>
+    </Card>
   );
 }
